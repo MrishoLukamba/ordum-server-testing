0af023b67e6178b26a6db7e208ad67444064d87b
Mrisho Lukamba <69342343+MrishoLukamba@users.noreply.github.com>
2023-07-10 16:43:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,51 @@
-const express = require("express");
+const express = require('express');
+const { spawn } = require('child_process');
+
 const app = express();
-const port = process.env.PORT || 3001;
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+let programProcess;
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+const startProgram = () => {
+  // Replace 'program-command' with the actual command to run your program
+  const programCommand = 'node my-program.js';
+
+  programProcess = spawn(programCommand);
+
+  programProcess.stdout.on('data', (data) => {
+    console.log(`Program stdout: ${data}`);
+  });
+
+  programProcess.stderr.on('data', (data) => {
+    console.error(`Program stderr: ${data}`);
+  });
+
+  programProcess.on('error', (error) => {
+    console.error(`Error executing program: ${error}`);
+  });
+
+  programProcess.on('close', (code) => {
+    console.log(`Program exited with code ${code}`);
+    programProcess = null;
+  });
+};
+
+const stopProgram = () => {
+  if (!programProcess) {
+    return;
+  }
+
+  programProcess.kill();
+  programProcess = null;
+};
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+
+  startProgram();
+});
+
+process.on('SIGINT', () => {
+  stopProgram();
+  process.exit();
+});
 
